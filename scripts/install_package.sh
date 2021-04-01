5b2830a5f306ded7ce72e768d96cd33c9d67b6b0
kolyandaemon <kolyan_08.08@mail.ru>
2021-04-01 20:34:50 +0300

    [add] vim package

--- scripts/install_package.sh
+++ scripts/install_package.sh
@@ -6,6 +6,6 @@ sudo yum makecache
 
 sudo sh /home/vagrant/scripts/cmake.sh --prefix=/usr/local  --exclude-subdir --skip-license
 
-sudo yum install -y kernel-devel ncurses-devel make gcc bc bison flex elfutils-libelf-devel openssl-devel grub2 wget rpm-build
+sudo yum install -y kernel-devel ncurses-devel make gcc bc bison flex elfutils-libelf-devel openssl-devel grub2 wget rpm-build vim
 
 sudo reboot
