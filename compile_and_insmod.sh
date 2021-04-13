029934caaf034646f732bda2d3f4a30975d44097
kolyandaemon <kolyan_08.08@mail.ru>
2021-04-13 23:17:04 +0300

    [fix] Vagrantfile for Windows 10

--- compile_and_insmod.sh
+++ compile_and_insmod.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cd /home/vagrant/linux-hevd-tutorial/builder
+cd /home/vagrant/linux-hevd/builder
 
 sh ./Build_HEVD_Vulnerable_x64.sh
 
-sudo insmod /home/vagrant/linux-hevd-tutorial/output/vulnerable/x64/hevd.ko
+sudo insmod /home/vagrant/linux-hevd/output/vulnerable/x64/hevd.ko
 
 sudo chmod 777 /dev/hevd
 
--- scripts/install_package.sh
+++ scripts/install_package.sh
@@ -4,12 +4,14 @@ sudo yum -y  update
 
 sudo yum makecache
 
-sudo sh /home/vagrant/scripts/cmake.sh --prefix=/usr/local  --exclude-subdir --skip-license
+sudo sh /home/vagrant/linux-hevd/scripts/cmake.sh --prefix=/usr/local  --exclude-subdir --skip-license
 
 sudo yum install -y kernel-devel ncurses-devel make gcc bc bison flex elfutils-libelf-devel openssl-devel grub2 wget rpm-build vim
 
 sudo yum install -y epel-release python36
 
- pip3.6 install --upgrade pip
+pip3.6 install --upgrade pip
+
+pip3.6 install -r /home/vagrant/linux-hevd/exploit/requirements.txt
 
 sudo reboot
